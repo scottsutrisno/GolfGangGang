81131ef06cf6aa0f854dd91fad720b270cba8373
Scott Sutrisno <scottsutrisno@gmail.com>
2018-06-11 14:55:05 -0400

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
 20.times do |n|
-    email =  "example#{n+1}@example.com"
+    email =  'example#{n+1}@example.com'
+    username = 'example#{n+1}'
     password = "password"
     User.create!(
         email: email,
+        username: username,
         password: password,
         password_confirmation: password)
 
 end
+
+User.create!(
+    email: 'scottsutrisno@gmail.com',
+    username: 'scottie',
+    password: '123123',
+    password_confirmation: '123123')
